9b648147a8571de5d1703f16999eba4230b19ba9
Lucas Delobelle <lucas@ld-web.net>
2024-08-07 14:38:49 +0200

    :sparkles: Articles list

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/articles', name: 'articles_list')]
+    public function list(ArticleRepository $articleRepository): Response
+    {
+        $articles = $articleRepository->findAll();
+
+        return $this->render('article/list.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+}
