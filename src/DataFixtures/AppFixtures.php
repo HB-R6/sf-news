b26406acb5dd821e3b7b3b8b669cf9adfde664d8
Lucas Delobelle <lucas@ld-web.net>
2024-08-07 10:17:45 +0200

    :sparkles: Fixtures : ORM Integration

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,7 @@ use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Faker\ORM\Doctrine\Populator;
 
 class AppFixtures extends Fixture
 {
@@ -18,30 +19,23 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('zh_TW');
 
-        $categories = [];
+        $categoryNameIdx = 0;
 
-        // --- CATEGORIES ----------------------------------------------
-        foreach (self::CATEGORIES_NAMES as $categoryName) {
-            $category = new Category();
-            $category->setName($categoryName);
-
-            $manager->persist($category);
-            $categories[] = $category;
-        }
-
-        // --- ARTICLES ------------------------------------------------
-        for ($i = 0; $i < self::NB_ARTICLES; $i++) {
-            $article = new Article();
-            $article
-                ->setTitle($faker->realTextBetween(9, 15))
-                ->setContent($faker->realTextBetween(350, 700))
-                ->setCreatedAt($faker->dateTimeBetween('-4 years'))
-                ->setVisible($faker->boolean(80))
-                ->setCategory($faker->randomElement($categories));
-
-            $manager->persist($article);
-        }
-
-        $manager->flush();
+        $populator = new Populator($faker, $manager);
+        $populator->addEntity(Category::class, count(self::CATEGORIES_NAMES), [
+            'name' => function () use (&$categoryNameIdx) {
+                return self::CATEGORIES_NAMES[$categoryNameIdx++];
+            }
+        ]);
+        $populator->addEntity(Article::class, self::NB_ARTICLES, [
+            'title' => function () use ($faker) {
+                return $faker->realTextBetween(9, 15);
+            },
+            'content' => function () use ($faker) {
+                return $faker->realTextBetween(150, 350);
+            },
+            'updatedAt' => null
+        ]);
+        $populator->execute();
     }
 }
