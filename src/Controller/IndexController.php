bb361996e33f2140f4c40bfbaded5ee94ba09ec2
Lucas Delobelle <lucas@ld-web.net>
2024-08-08 15:10:52 +0200

    :sparkles: add Categories on homepage

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,17 @@ use Symfony\Component\Routing\Attribute\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function home(): Response
+    public function home(CategoryRepository $categoryRepository): Response
     {
+        // 1 - Je requête le modèle (SQL/BDD)
+        // pour récupérer les catégories
+        $categories = $categoryRepository->findAll();
+
+        // 2 - Je demande à Twig de rendre une vue
+        // et je lui passe les catégories
+        // Répertoire racine des vues : templates/
         return $this->render('index/index.html.twig', [
-            'student_name' => 'Evan',
+            'categories' => $categories,
         ]);
     }
 
