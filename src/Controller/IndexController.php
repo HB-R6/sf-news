14dd99d874d61ef120f499edbfe26258fa94dbfc
Lucas Delobelle <lucas@ld-web.net>
2024-08-09 10:03:43 +0200

    :sparkles: add Newsletter Form

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\NewsletterEmail;
+use App\Form\NewsletterType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -28,5 +32,35 @@ class IndexController extends AbstractController
     public function about(): Response
     {
         return $this->render('index/about.html.twig');
+    }
+
+    #[Route('/newsletter/subscribe', name: "newsletter_subscribe", methods: ["GET", "POST"])]
+    public function newsletterSubscribe(
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        $newsletter = new NewsletterEmail();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+
+        // Prend en charge la requête entrante
+        // et s'il y a des données, les met dans $newsletter
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($newsletter);
+            $em->flush();
+
+            return $this->redirectToRoute('newsletter_confirm');
+        }
+
+        return $this->render('index/newsletter.html.twig', [
+            'newsletterForm' => $form
+        ]);
+    }
+
+    #[Route('/newsletter/thanks', name: "newsletter_confirm")]
+    public function newsletterConfirm(): Response
+    {
+        return $this->render('index/newsletter_confirm.html.twig');
     }
 }
